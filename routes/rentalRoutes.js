139909ae67ed17d0f8d14f4b67fca20ffec560af
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-04-06 02:07:26 +0545

    rental free

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -4,17 +4,27 @@ const rentalController = require('./../controllers/rentalController');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 
-router.use(authController.protect);
-
 router
   .route('/')
   .get(rentalController.getAllRentals)
-  .post(authController.restrictTo('user'), rentalController.createRental);
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    rentalController.createRental
+  );
 
 router
   .route('/:id')
   .get(rentalController.getRental)
-  .patch(authController.restrictTo('admin'), rentalController.updateRental)
-  .delete(authController.restrictTo('admin'), rentalController.deleteRental);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    rentalController.updateRental
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    rentalController.deleteRental
+  );
 
 module.exports = router;
