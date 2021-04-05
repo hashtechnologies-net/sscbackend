9d41b024e2f96146d537df572b744b8292bfb5ac
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-04-06 02:59:20 +0545

    added home comment

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,6 @@
 exports.getHome = (req, res, next) => {
-  const home = 'welcome to SSC API';
+  const home =
+    'Welcome to SSC API, This is an API for Health Wealth Card Mobile Application';
 
   return res.status(200).json({ status: 'success', data: home });
 };
