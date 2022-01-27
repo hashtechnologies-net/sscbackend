fad36fe8e34eca4d78314806c7941a2b24b87ee2
ramankarki <ramankarki40@gmail.com>
2022-01-27 15:10:27 +0545

    fix morgan and dependencies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const compression = require('compression');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +23,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV == 'development') {
+  const morgan = require('morgan');
   app.use(morgan('dev'));
 }
 
